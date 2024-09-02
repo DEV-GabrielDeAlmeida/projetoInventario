ae88e572e7e4f8ad28ee255078135c119dbd1f8d
Gabriel De Almeida Silva <gabrielsilvadev.dev@gmail.com>
2024-09-01 23:43:58 -0300

    aplicar mediaQuery

--- assets/js/openMenu.js
+++ assets/js/openMenu.js
@@ -1,24 +1,44 @@
 openMenu.addEventListener("click", () => {
-    menu.style.display = "flex";
+  menu.style.display = "flex";
 
-    menu.style.right = menu.offsetWidth * -1 + "px";
+  menu.style.right = menu.offsetWidth * -1 + "px";
 
-    openMenu.style.display = "none";
+  openMenu.style.display = "none";
 
-    setTimeout(() => {
-        menu.style.opacity = "1";
+  document
+    .querySelectorAll(
+      ".qrCodeBtn, .tableEditIcon, .tableDeleteIcon, .switch, .search, .search_icon, .novoChamadoButton, .novoAtivoButton, .novoUsuarioButton"
+    )
+    .forEach((icon) => {
+      icon.style.display = "none";
+    });
 
-        menu.style.right = "0";
-    }, 10);
+  //   qrCodeBtn.style.display = "none";
+  //   tableEditIcon.style.display = "none";
+  //   tableDeleteIcon.style.display = "none";
+
+  setTimeout(() => {
+    menu.style.opacity = "1";
+
+    menu.style.right = "0";
+  }, 10);
 });
 
 closeMenu.addEventListener("click", () => {
-    menu.style.opacity = "0";
+  menu.style.opacity = "0";
 
-    menu.style.right = menu.offsetWidth * -1 + "px";
+  document
+    .querySelectorAll(
+      ".qrCodeBtn, .tableEditIcon, .tableDeleteIcon, .switch, .search, .search_icon, .novoChamadoButton, .novoAtivoButton, .novoUsuarioButton"
+    )
+    .forEach((icon) => {
+      icon.removeAttribute("style");
+    });  
 
-    setTimeout(() => {
-        menu.removeAttribute("style");
-        openMenu.removeAttribute("style");
-    }, 200);
+  menu.style.right = menu.offsetWidth * -1 + "px";
+
+  setTimeout(() => {
+    menu.removeAttribute("style");
+    openMenu.removeAttribute("style");
+  }, 200);
 });
